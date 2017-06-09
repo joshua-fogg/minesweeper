4cb678bbce2833836392b45855874c4ea73740ae
Joshua Fogg <joshuafoggnz@gmail.com>
2017-06-09 14:10:04 +1200

    added board.cells.ismarked

--- minesweeper.js
+++ minesweeper.js
@@ -3,31 +3,31 @@ document.addEventListener('DOMContentLoaded', startGame)
 // Define your `board` object here!
 var board = {
   cells:[
-    { row: 0, col: 0, isMine: true , hidden: true, surroundingMines: 0},
-    { row: 0, col: 1, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 0, col: 2, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 0, col: 3, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 0, col: 4, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 1, col: 0, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 1, col: 1, isMine: true,  hidden: true, surroundingMines: 0},
-    { row: 1, col: 2, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 1, col: 3, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 1, col: 4, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 2, col: 0, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 2, col: 1, isMine: true,  hidden: true, surroundingMines: 0},
-    { row: 2, col: 2, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 2, col: 3, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 2, col: 4, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 3, col: 0, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 3, col: 1, isMine: true,  hidden: true, surroundingMines: 0},
-    { row: 3, col: 2, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 3, col: 3, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 3, col: 4, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 4, col: 0, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 4, col: 1, isMine: true,  hidden: true, surroundingMines: 0},
-    { row: 4, col: 2, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 4, col: 3, isMine: false, hidden: true, surroundingMines: 0},
-    { row: 4, col: 4, isMine: false, hidden: true, surroundingMines: 0}
+    { row: 0, col: 0, isMine: true , hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 0, col: 1, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 0, col: 2, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 0, col: 3, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 0, col: 4, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 1, col: 0, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 1, col: 1, isMine: true,  hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 1, col: 2, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 1, col: 3, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 1, col: 4, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 2, col: 0, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 2, col: 1, isMine: true,  hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 2, col: 2, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 2, col: 3, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 2, col: 4, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 3, col: 0, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 3, col: 1, isMine: true,  hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 3, col: 2, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 3, col: 3, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 3, col: 4, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 4, col: 0, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 4, col: 1, isMine: true,  hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 4, col: 2, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 4, col: 3, isMine: false, hidden: true, surroundingMines: 0, isMarked: false},
+    { row: 4, col: 4, isMine: false, hidden: true, surroundingMines: 0, isMarked: false}
   ]
 }
 
@@ -38,7 +38,7 @@ function startGame () {
   }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
-  
+
 }
 
 // Define this function to look for a win condition:
